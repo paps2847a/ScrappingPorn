4706d77dfa26c389f3ec72eeb90043afa7def55c
paps2847a <rierapablo577@gmail.com>
2024-10-12 18:55:31 -0400

    Agregar archivos de proyecto.

---
+++ ScrappingPorn/Program.cs
@@ -0,0 +1,252 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ScrappingPorn
+{
+    internal class Program
+    {
+        public static Action<Object> Print = (Object x) => Console.WriteLine(x);
+
+        static async Task Main(string[] args)
+        {
+            var path = new PathHandler();
+            var imgControl = new ImageDownloader(path);
+
+            Print("Bienvenido animal de mierda que deseas hacer?");
+            Print("");
+            Print("");
+            Print("Opciones:");
+            Print("");
+
+            while (true)
+            {
+                Print("1) Establecer direccion de guardado de imagenes");
+                Print("2) Descargar Imagenes");
+                Print("3) Cerrar Programa");
+                Print("");
+                var opcion = int.TryParse(Console.ReadLine(), out int value);
+                if (!opcion)
+                {
+                    Print("no es una opcion valida pendejo");
+                    continue;
+                }
+
+                if(value > 3 || value == 0)
+                {
+                    Print("No hay opcion con ese numero imbecil");
+                    continue;
+                }
+
+                if(value == 1)
+                {
+                    Print("Estas son las opciones de manejo del destino de guardado, que quieres hacer?:");
+                    Print("1) Crear Path");
+                    Print("2) Ver Path");
+                    Print("3) Editar Path");
+
+                    var opcionPath = int.TryParse(Console.ReadLine(), out int valuePath);
+                    switch(valuePath)
+                    {
+                        case 1:
+                            if (path.CreatePathFile())
+                            {
+                                Print("Path Creado");
+                                Print("");
+                            }
+                            break;
+                        case 2:
+                            if (path.GetPath() == string.Empty)
+                            {
+                                Print("O no existe el archivo o no tiene nada dentro, imbecil");
+                                Print("");
+                            }
+                            else
+                            {
+                                Print($"DIRECCION DE PATH: {path.GetPath()}");
+                                Print("");
+                            }
+                            break;
+                        case 3:
+                            Print("Pon la direccion base en donde quieres crear las carpetas con tus cochinadas, procura poner solo la direccion base:");
+                            if (path.SavePath(Console.ReadLine()) != string.Empty)
+                            {
+                                Print("Direccion Creada");
+                                Print("");
+                            }
+                            else
+                            {
+                                Print("Hubo un error");
+                                Print("");
+                            }
+                            break;
+                        default:
+                            Print("No existe tal opcion");
+                            break;
+                    }
+                }
+
+                if(value == 2)
+                {
+                    Print("Hora de la accion nena");
+                    Print("Pon la URL del sitio, el nombre de la carpeta y el rango de imagenes que quieres. Todo separado por una coma ','");
+                    var rawData = Console.ReadLine()?.Split(",");
+                    try
+                    {
+                        if (rawData == null || rawData.Length != 4)
+                            throw new Exception("Problema de data");
+
+                        var polnoResult = await imgControl.DownlaodImgs(rawData[0], rawData[1], int.Parse(rawData[2]), int.Parse(rawData[3]));
+                        if(polnoResult)
+                        {
+                            Print("");
+                            Print("Hora de la paja");
+                            Print("");
+                        }
+                        else
+                        {
+                            Print("");
+                            Print("Hubo un problema...");
+                            Print("Noooo mi polnoooooo");
+                            Print("");
+                        }
+                    }
+                    catch (Exception e) {
+                        Print("Hubo un problema con los datos que diste");
+                    }
+                }
+
+                if(value == 3)
+                {
+                    Print("Gracias por hacerme perder mi tiempo imbecil");
+                    Print("Presiona cualquier tecla para cerrar");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+        }
+    }
+
+    public class ImageDownloader
+    {
+        private readonly HttpClient _client;
+        private IPathHandler _pathHandler;
+
+        public ImageDownloader(IPathHandler pathHandler)
+        {
+            _client = new HttpClient();
+            _pathHandler = pathHandler;
+        }
+
+        public async Task<bool> DownlaodImgs(string urlToDownload, string carpeta, int minRange, int maxRange)
+        {
+            try
+            {
+                //var urlBase = "https://m9.imhentai.xxx/028/ox1ve25iar/";
+                var descargas = new List<Task>();
+                if (!Directory.Exists($"{_pathHandler.GetPath()}\\{carpeta}"))
+                    Directory.CreateDirectory($"{_pathHandler.GetPath()}\\{carpeta}");
+
+                var builder = new StringBuilder($"{_pathHandler.GetPath()}\\{carpeta}");
+
+                for (var init = minRange; init < maxRange; ++init)
+                {
+                    var rawData = await _client.GetStreamAsync($"{urlToDownload}{init}.jpg");
+                    builder.Append($"\\{init}.jpg");
+
+                    var filestream = new FileStream(builder.ToString(), FileMode.Create);
+                    descargas.Add(rawData.CopyToAsync(filestream));
+
+                    var cleanPath = builder.RemoveLastImage();
+                    builder.Clear().Append(cleanPath);
+                }
+
+                await Task.WhenAll(descargas);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        } 
+
+
+    }
+
+    public interface IPathHandler
+    {
+        string GetPath();
+    }
+    public class PathHandler : IPathHandler
+    {
+        private string AppContx = $"{AppContext.BaseDirectory}\\FilePATH\\direction.txt";
+
+        public bool CreatePathFile()
+        {
+            try
+            {
+                var pathAndFile = AppContx;
+                if (File.Exists(pathAndFile))
+                {
+                    return false;
+                }
+                else
+                {
+                    File.Create(pathAndFile).Close();
+                    File.WriteAllText(pathAndFile, "aqui se sobreescribira con la direccion de guardado que quieras");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string SavePath(string? path)
+        {
+            try
+            {
+                if (path == null)
+                    return string.Empty;
+
+                File.Create(AppContx).Close();
+                File.WriteAllText(AppContx, path);
+                return "exito";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string GetPath()
+        {
+            try
+            {
+                return File.ReadAllText(AppContx);
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    public static class ExtensionFunctionsBuilder
+    {
+        public static String RemoveLastImage(this StringBuilder builder)
+        {
+            try
+            {
+                var data = builder.ToString().Split("\\").ToList();
+                data.RemoveAt(data.Count - 1);
+
+                return String.Join("\\", data);
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
